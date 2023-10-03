b87141654a7b097ff6a0c36ce9bcf300a9c73017
Adyl <you@example.com>
2023-10-03 15:30:17 +0600

    a

--- backend/apps/products/urls.py
+++ backend/apps/products/urls.py
@@ -16,12 +16,12 @@ router = DefaultRouter()
 router.register("categories", CategoryViewSet, basename="categories")
 router.register("tags", TagViewSet, basename="tags")
 router.register("specifications", SpecificationViewSet, basename="specifications")
-router.register("customer/products", ProductCustomerViewSet, basename="customer_products")
-router.register("customer/favourites", ProductFavouriteViewSet, basename="customer_favourites")
-router.register("customer/reviews", ProductReviewViewSet, basename="product_reviews")
-router.register("seller/products", ProductSellerViewSet, basename="seller_products")
-router.register("seller/variants", ProductVariantSellerViewSet, basename="seller_variants")
-router.register("seller/variants/images", ProductVariantImageSellerViewSet, basename="seller-variants-images")
+router.register("customer-products", ProductCustomerViewSet, basename="customer_products")
+router.register("customer-favourites", ProductFavouriteViewSet, basename="customer_favourites")
+router.register("customer-reviews", ProductReviewViewSet, basename="product_reviews")
+router.register("seller-products", ProductSellerViewSet, basename="seller_products")
+router.register("seller-variants", ProductVariantSellerViewSet, basename="seller_variants")
+router.register("seller-variants-images", ProductVariantImageSellerViewSet, basename="seller-variants-images")
 
 urlpatterns = []
 
