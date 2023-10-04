83853e2b967c6ab393bb2b510d1f0da257f14a08
Adyl <you@example.com>
2023-10-04 18:11:22 +0600

    fix me

--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -69,7 +69,7 @@ class UserViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         raise MethodNotAllowed('POST')
 
-    def get_instance(self):
+    def get_object(self):
         return self.request.user
 
     @action(['post'], detail=False)
@@ -103,8 +103,6 @@ class UserViewSet(viewsets.ModelViewSet):
 
     @action(["get", "put", "patch", "delete"], detail=False)
     def me(self, request, *args, **kwargs):
-        self.get_object = self.get_instance()
-
         if request.method == "GET":
             return self.retrieve(request, *args, **kwargs)
         elif request.method == "PUT":
